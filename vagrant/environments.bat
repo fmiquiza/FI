d1c2761edf4ef5add47b012f2dabb053c3c0696c
fmiquiza <fmiquiza@gmail.com>
2018-06-21 17:13:53 -0400

    update environments file

--- vagrant/environments.bat
+++ vagrant/environments.bat
@@ -2,10 +2,10 @@
 set OS_AUTH_URL=http://10.28.3.240:5000/v3
 set OS_TENANT_NAME=51691ab150af40c8a7be5ee425989549
 set OS_INITIALS=FIR
-set OS_USERNAME=Fernando Iquiza
-set OS_PASSWORD=ATat676435019
-set OS_PRIVATE_KEY_PATH=C:/Users/devops/.ssh/id_rsa
-set OS_KEY_PAIR_NAME=keyFher
+set OS_USERNAME=
+set OS_PASSWORD=
+set OS_PRIVATE_KEY_PATH=C:/Users/devops/id_rsa.ppk
+set OS_KEY_PAIR_NAME=keyFherDH
 set OS_DOMAIN_NAME=jalasoft
 set OS_PROJECT_NAME=jalasoft_devops101
 set OS_IMAGE=ubuntu-16.04-amd64-server_17052018
